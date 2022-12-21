8c9575033c12691465377ad25ab76beae4fdd7c7
Anubhav1603 <kishansingh2575@gmail.com>
2022-12-21 23:48:51 +0530

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,24 +9,25 @@ function sendMoney() {
   if (enterAmount > 8000) {
     alert("Insufficient Balance.");
   } else {
-    var findUserBankAccount = enterName.replace(/\W+/g,"").toLowerCase() + "BankBalance";
-    console.log(findUserBankAccount)
+    var findUserBankAccount =
+      enterName.replace(/\W+/g, "").toLowerCase() + "BankBalance";
+    console.log(findUserBankAccount);
     var finalAmount =
       parseInt(document.getElementById(findUserBankAccount).innerHTML) +
       enterAmount;
-      console.log(finalAmount)
+    console.log(finalAmount);
     var myAccountBalance =
       parseInt(document.getElementById("myAccountBalance").innerText) -
       enterAmount;
     document.getElementById("myAccountBalance").innerText = myAccountBalance;
     document.getElementById(findUserBankAccount).innerHTML = finalAmount;
     alert(`Successful Transaction !!
-      $${enterAmount} is sent to ${enterName}@email.com.`);
+      $${enterAmount} is sent to ${enterName}`);
 
     // transaction history
     var createPTag = document.createElement("li");
     var textNode = document.createTextNode(
-      `$${enterAmount} is sent to recepient with Email-id ${enterName}@email.com on ${Date()}.`
+      `$${enterAmount} is sent to recepient ${enterName} on ${Date()}.`
     );
     createPTag.appendChild(textNode);
     var element = document.getElementById("transaction-history-body");
@@ -40,5 +41,4 @@ let http = new XMLHttpRequest();
 http.open("GET", "http://127.0.0.1:8000/");
 http.send();
 const text = (http.onload = () => console.log(http.responseText));
-console.log
-
+console.log;
